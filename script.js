8e049671580e227cc9f1d552b91513cf3a008b69
lgmacedo <lucasguerra.macedo@gmail.com>
2023-01-04 13:51:35 -0300

    botão de finalizar pedido

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let pratoSelecionado = 0;
+let bebidaSelecionada = 0;
+let sobremesaSelecionada = 0;
+
+function terminouPedido(){
+    if((pratoSelecionado !== 0) && (bebidaSelecionada !== 0) && (sobremesaSelecionada !== 0)){
+        document.querySelector('footer button').disabled = false;
+        document.querySelector('footer button').classList.add('backgroundVerdeClicavel');
+        document.querySelector('footer button').innerHTML = "<p>Fechar pedido</p>";
+    }
+}
+
 function marcaPrato(numPrato){
     if (numPrato === 1){
         document.querySelector(".pratos>div:nth-child(2)").classList.remove('bordaVerde');
@@ -8,7 +20,8 @@ function marcaPrato(numPrato){
 
         document.querySelector(".pratos>div:nth-child(1)").classList.add('bordaVerde');
         document.querySelector(".pratos>div:nth-child(1) ion-icon").classList.add('checkVerde');
-        return numPrato;
+        pratoSelecionado = 1;
+        terminouPedido();
 
     } else if (numPrato === 2){
         document.querySelector(".pratos>div:nth-child(1)").classList.remove('bordaVerde');
@@ -19,7 +32,8 @@ function marcaPrato(numPrato){
 
         document.querySelector(".pratos>div:nth-child(2)").classList.add('bordaVerde');
         document.querySelector(".pratos>div:nth-child(2) ion-icon").classList.add('checkVerde');
-        return numPrato;
+        pratoSelecionado = 2;
+        terminouPedido();
 
     } else if (numPrato === 3){
         document.querySelector(".pratos>div:nth-child(1)").classList.remove('bordaVerde');
@@ -30,7 +44,8 @@ function marcaPrato(numPrato){
 
         document.querySelector(".pratos>div:nth-child(3)").classList.add('bordaVerde');
         document.querySelector(".pratos>div:nth-child(3) ion-icon").classList.add('checkVerde');
-        return numPrato;
+        pratoSelecionado = 3;
+        terminouPedido();
     }
 }
 
@@ -44,7 +59,8 @@ function marcaBebida(numPrato){
 
         document.querySelector(".bebidas>div:nth-child(1)").classList.add('bordaVerde');
         document.querySelector(".bebidas>div:nth-child(1) ion-icon").classList.add('checkVerde');
-        return numPrato;
+        bebidaSelecionada = 1;
+        terminouPedido();
 
     } else if (numPrato === 2){
         document.querySelector(".bebidas>div:nth-child(1)").classList.remove('bordaVerde');
@@ -55,7 +71,8 @@ function marcaBebida(numPrato){
 
         document.querySelector(".bebidas>div:nth-child(2)").classList.add('bordaVerde');
         document.querySelector(".bebidas>div:nth-child(2) ion-icon").classList.add('checkVerde');
-        return numPrato;
+        bebidaSelecionada = 2;
+        terminouPedido();
 
     } else if (numPrato === 3){
         document.querySelector(".bebidas>div:nth-child(1)").classList.remove('bordaVerde');
@@ -66,7 +83,8 @@ function marcaBebida(numPrato){
 
         document.querySelector(".bebidas>div:nth-child(3)").classList.add('bordaVerde');
         document.querySelector(".bebidas>div:nth-child(3) ion-icon").classList.add('checkVerde');
-        return numPrato;
+        bebidaSelecionada = 3;
+        terminouPedido();
     }
 }
 
@@ -80,7 +98,8 @@ function marcaSobremesa(numPrato){
 
         document.querySelector(".sobremesas>div:nth-child(1)").classList.add('bordaVerde');
         document.querySelector(".sobremesas>div:nth-child(1) ion-icon").classList.add('checkVerde');
-        return numPrato;
+        sobremesaSelecionada = 1;
+        terminouPedido();
 
     } else if (numPrato === 2){
         document.querySelector(".sobremesas>div:nth-child(1)").classList.remove('bordaVerde');
@@ -91,7 +110,8 @@ function marcaSobremesa(numPrato){
 
         document.querySelector(".sobremesas>div:nth-child(2)").classList.add('bordaVerde');
         document.querySelector(".sobremesas>div:nth-child(2) ion-icon").classList.add('checkVerde');
-        return numPrato;
+        sobremesaSelecionada = 2;
+        terminouPedido();
 
     } else if (numPrato === 3){
         document.querySelector(".sobremesas>div:nth-child(1)").classList.remove('bordaVerde');
@@ -102,6 +122,11 @@ function marcaSobremesa(numPrato){
 
         document.querySelector(".sobremesas>div:nth-child(3)").classList.add('bordaVerde');
         document.querySelector(".sobremesas>div:nth-child(3) ion-icon").classList.add('checkVerde');
-        return numPrato;
+        sobremesaSelecionada = 3;
+        terminouPedido();
     }
+}
+
+function fecharPedido(){
+    document.querySelector('footer button').classList.add('backgroundVermelho');
 }
