10c67e55e9ff948965b76b57cc5c2cc2d801cf27
lgmacedo <lucasguerra.macedo@gmail.com>
2023-01-04 15:08:14 -0300

    envio do pedido - projeto finalizado, sem bônus

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ let pratoSelecionado = 0;
 let bebidaSelecionada = 0;
 let sobremesaSelecionada = 0;
 
+let nomePrato = "";
+let nomeBebida = "";
+let nomeSobremesa = "";
+
+let valorPrato = 0;
+let valorBebida = 0;
+let valorSobremesa = 0;
+
+let valorFinal = 0;
+
 function terminouPedido(){
     if((pratoSelecionado !== 0) && (bebidaSelecionada !== 0) && (sobremesaSelecionada !== 0)){
         document.querySelector('footer button').disabled = false;
@@ -21,6 +31,8 @@ function marcaPrato(numPrato){
         document.querySelector(".pratos>div:nth-child(1)").classList.add('bordaVerde');
         document.querySelector(".pratos>div:nth-child(1) ion-icon").classList.add('checkVerde');
         pratoSelecionado = 1;
+        nomePrato = "Frango Yin Yang";
+        valorPrato = 14.90;
         terminouPedido();
 
     } else if (numPrato === 2){
@@ -33,6 +45,8 @@ function marcaPrato(numPrato){
         document.querySelector(".pratos>div:nth-child(2)").classList.add('bordaVerde');
         document.querySelector(".pratos>div:nth-child(2) ion-icon").classList.add('checkVerde');
         pratoSelecionado = 2;
+        nomePrato = "Hambúrguer";
+        valorPrato = 16.90;
         terminouPedido();
 
     } else if (numPrato === 3){
@@ -45,6 +59,8 @@ function marcaPrato(numPrato){
         document.querySelector(".pratos>div:nth-child(3)").classList.add('bordaVerde');
         document.querySelector(".pratos>div:nth-child(3) ion-icon").classList.add('checkVerde');
         pratoSelecionado = 3;
+        nomePrato = "Sushi";
+        valorPrato = 24.90;
         terminouPedido();
     }
 }
@@ -60,6 +76,8 @@ function marcaBebida(numPrato){
         document.querySelector(".bebidas>div:nth-child(1)").classList.add('bordaVerde');
         document.querySelector(".bebidas>div:nth-child(1) ion-icon").classList.add('checkVerde');
         bebidaSelecionada = 1;
+        nomeBebida = "Coquinha gelada";
+        valorBebida = 4.90;
         terminouPedido();
 
     } else if (numPrato === 2){
@@ -72,6 +90,8 @@ function marcaBebida(numPrato){
         document.querySelector(".bebidas>div:nth-child(2)").classList.add('bordaVerde');
         document.querySelector(".bebidas>div:nth-child(2) ion-icon").classList.add('checkVerde');
         bebidaSelecionada = 2;
+        nomeBebida = "Milkshake";
+        valorBebida = 6.90;
         terminouPedido();
 
     } else if (numPrato === 3){
@@ -84,6 +104,8 @@ function marcaBebida(numPrato){
         document.querySelector(".bebidas>div:nth-child(3)").classList.add('bordaVerde');
         document.querySelector(".bebidas>div:nth-child(3) ion-icon").classList.add('checkVerde');
         bebidaSelecionada = 3;
+        nomeBebida = "Suco natural";
+        valorBebida = 5.90;
         terminouPedido();
     }
 }
@@ -99,6 +121,8 @@ function marcaSobremesa(numPrato){
         document.querySelector(".sobremesas>div:nth-child(1)").classList.add('bordaVerde');
         document.querySelector(".sobremesas>div:nth-child(1) ion-icon").classList.add('checkVerde');
         sobremesaSelecionada = 1;
+        nomeSobremesa = "Pudim";
+        valorSobremesa = 7.90;
         terminouPedido();
 
     } else if (numPrato === 2){
@@ -111,6 +135,8 @@ function marcaSobremesa(numPrato){
         document.querySelector(".sobremesas>div:nth-child(2)").classList.add('bordaVerde');
         document.querySelector(".sobremesas>div:nth-child(2) ion-icon").classList.add('checkVerde');
         sobremesaSelecionada = 2;
+        nomeSobremesa = "Brownie no pote";
+        valorSobremesa = 11.90;
         terminouPedido();
 
     } else if (numPrato === 3){
@@ -123,10 +149,16 @@ function marcaSobremesa(numPrato){
         document.querySelector(".sobremesas>div:nth-child(3)").classList.add('bordaVerde');
         document.querySelector(".sobremesas>div:nth-child(3) ion-icon").classList.add('checkVerde');
         sobremesaSelecionada = 3;
+        nomeSobremesa = "Sorvete";
+        valorSobremesa = 9.90;
         terminouPedido();
     }
 }
 
 function fecharPedido(){
-    document.querySelector('footer button').classList.add('backgroundVermelho');
+    valorFinal = valorPrato + valorBebida + valorSobremesa;
+    const valorFinalCorrigido = valorFinal.toFixed(2);
+    const mensagem = "Olá, gostaria de fazer o pedido: \n- Prato: "+nomePrato+"\n- Bebida: "+nomeBebida+"\n- Sobremesa: "+nomeSobremesa+" \nTotal: R$ "+valorFinalCorrigido;
+    const mensagemCorrigida = encodeURIComponent(mensagem);
+    open("https://wa.me/5532998382271?text="+mensagemCorrigida);
 }
